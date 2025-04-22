86d8a10c5502e4d18fddba35b10edfb2f9e1aeca
micknin <erik.fujihira.moreira@gmail.com>
2025-04-21 21:22:33 -0300

    Update chrRAM assembler.bat

--- chrRAM assembler.bat
+++ chrRAM assembler.bat
@@ -1,5 +1,5 @@
 mode 128,32
-::you can change the file path to wherever you want, as long as the .s file is in the same folder
+::you can change the file path to wherever you want.
 C:\cc65\bin\ca65 C:\code\helloworldCHRRAM.s -o C:\code\helloworldCHRRAM.o -t nes
 C:\cc65\bin\ld65 C:\code\helloworldCHRRAM.o -o C:\code\helloworldCHRRAM.nes -t nes
 pause 
