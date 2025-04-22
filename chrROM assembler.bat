79eb51b8f2997d5bcd5eec1bcb10efe19a734a8c
micknin <erik.fujihira.moreira@gmail.com>
2025-04-21 21:22:17 -0300

    Update chrROM assembler.bat

--- chrROM assembler.bat
+++ chrROM assembler.bat
@@ -1,5 +1,5 @@
 mode 128,32
-::you can change the file path to wherever you want, as long as the .s file is in the same folder
+::you can change the file path to wherever you want.
 C:\cc65\bin\ca65 C:\code\helloworldCHRROM.s -o C:\code\helloworldCHRROM.o -t nes
 C:\cc65\bin\ld65 C:\code\helloworldCHRROM.o -o C:\code\helloworldCHRROM.nes -t nes
 pause 
